830a93f0bbe7eb5fa3bea704ecfafee2fcd31a0f
mangorex <mangorex89@gmail.com>
2022-02-17 09:20:24 +0100

    Point 1 completed without refactoring

--- codeTestTgx/Controllers/HotelsController.cs
+++ codeTestTgx/Controllers/HotelsController.cs
@@ -9,13 +9,16 @@ namespace codeTestTgx.Controllers
     [ApiController]
     public class HotelsController : ControllerBase
     {
+        #region Variables
         // Declaration of HttpClient global variable
         static HttpClient client = new HttpClient();
         // Declaration of strings path like a global variable
         static string pathHotelsAtalaya = "http://www.mocky.io/v2/5e4a7e4f2f00005d0097d253";
         static string pathRoomsAtalaya = "https://run.mocky.io/v3/132af02e-8beb-438f-ac6e-a9902bc67036";
-        static string pathMealPlanAtalaya = "http://www.mocky.io/v2/5e4a7e282f0000490097d252";
+        static string pathMPAtalaya = "http://www.mocky.io/v2/5e4a7e282f0000490097d252";
         static string pathHotelsApiResort = "http://www.mocky.io/v2/5e4e43272f00006c0016a52b";
+        static string pathRoomMPApiResort = "http://www.mocky.io/v2/5e4a7dd02f0000290097d24b";
+        #endregion
 
         #region Endpoints
 
@@ -44,7 +47,6 @@ namespace codeTestTgx.Controllers
         {
             // Get of hotels list in Atalaya format
             HttpResponseMessage responseHotels = await client.GetAsync(pathHotelsAtalaya);
-            int i = 0;
 
             if (responseHotels.IsSuccessStatusCode)
             {
@@ -52,22 +54,20 @@ namespace codeTestTgx.Controllers
                 HotelListTGX atalaya = await responseHotels.Content.ReadAsAsync<HotelListTGX>();
                 HttpResponseMessage responseRooms = await client.GetAsync(pathRoomsAtalaya);
                 RoomsTypes rooms = await responseRooms.Content.ReadAsAsync<RoomsTypes>();
-                HttpResponseMessage responseMealPlan = await client.GetAsync(pathMealPlanAtalaya);
+                HttpResponseMessage responseMPAta = await client.GetAsync(pathMPAtalaya);
+
                 // FAIL IS HERE. In this foreach We are calling 2 times to here
                 MealPlanAtalaya mealPlanAtalaya =
-                    await responseMealPlan.Content.ReadAsAsync<MealPlanAtalaya>();
+                    await responseMPAta.Content.ReadAsAsync<MealPlanAtalaya>();
 
-                if (responseMealPlan.IsSuccessStatusCode)
+                if (responseMPAta.IsSuccessStatusCode)
                 {
                     // Loop atalaya hotels to save the information in hotels list in TravelgateX format
                     foreach (HotelTGX hotel in atalaya.hotels)
                     {
                         HotelTGX hotelTGX = new HotelTGX(hotel.Code, hotel.Name, hotel.City);
                         GetRoomsAtalaya(hotelTGX, hotel, rooms, mealPlanAtalaya);
-
                         hotelsTGX.hotels.Add(hotelTGX);
-                        i++;
-                        Debug.WriteLine("GetHotelsAtalayaAsync i: " + i);
                     }
 
                     // FAIL HERE. Probablemente haya que llamar aqui al meal plan
@@ -81,8 +81,6 @@ namespace codeTestTgx.Controllers
         private void GetRoomsAtalaya(HotelTGX hotelTGX, HotelTGX hotel,
             RoomsTypes rooms, MealPlanAtalaya mealPlanAtalaya)
         {
-            int iroomType = 0, ihotelCode = 0;
-
             // Loop to save roooms in TravelgateX format
             foreach (RoomsType roomType in rooms.rooms_type)
             {
@@ -90,67 +88,52 @@ namespace codeTestTgx.Controllers
                 {
                     if (hotelCode.Equals(hotel.Code))
                     {
-                        // RoomTGX roomTGX = new RoomTGX(roomType);
-
                         GetMealPlansInRoomsAtalaya(hotelTGX, hotel, // roomTGX,
                             mealPlanAtalaya, roomType);
                     }
-
-                    // hotelTGX.Rooms.Add(roomTGX);
-                    ihotelCode++;
-                    Debug.WriteLine("GetRoomsAtalaya i hotelCode: " + ihotelCode);
                 }
 
             }
-            iroomType++;
-            Debug.WriteLine("GetRoomsAtalaya i roomType: " + iroomType);
         }
 
         // This functin get meal plans and duplicate rooms saving code mealplan and price
-        // REVIEW THIS CODE
         private void GetMealPlansInRoomsAtalaya(HotelTGX hotelTGX, HotelTGX hotel, // RoomTGX roomTGX,
             MealPlanAtalaya mealPlanAtalaya, RoomsType roomType)
         {
-            int iMealPlan = 0;
             RoomTGX roomTGX = null;
 
             foreach (MealPlan mealPlan in mealPlanAtalaya.meal_plans)
-            // for (int iMealPlan = 0; iMealPlan < mealPlanAtalaya.meal_plans.Count(); iMealPlan++)
             {
-                // MealPlan mealPlan = mealPlanAtalaya.meal_plans[iMealPlan];
                 if (nameof(mealPlan.hotel.acs).Equals(hotel.Code))
                 {
                     string codeMealPlan = mealPlan.code;
                     for (int i = 0; i < mealPlan.hotel.acs.Count(); i++)
                     {
-                        roomTGX = new RoomTGX(roomType);
-                        Acs acs = mealPlan.hotel.acs[i];
-                        // Acs acs = mealPlan.hotel.acs.FirstOrDefault();
-                        roomTGX.Price = acs.price;
-                        roomTGX.Meals_plan = codeMealPlan;
-                        roomTGX.Room_type = acs.room.ToUpper().Equals(
-                            Room_Type.Suite.ToString().ToUpper())
-                            ? Room_Type.Suite : Room_Type.Standard;
-                        hotelTGX.Rooms.Add(roomTGX);
-                        // var itemToRemove = mealPlan.hotel.ave.Find(m => m.price == acs.price && m.room == acs.room);
-                        // mealPlan.hotel.ave.Remove(itemToRemove);
+                        if (roomType.code.Equals(mealPlan.hotel.acs[i].room))
+                        {
+                            roomTGX = new RoomTGX(roomType);
+                            Acs acs = mealPlan.hotel.acs[i];
+                            roomTGX.Price = acs.price;
+                            roomTGX.Meals_plan = codeMealPlan;
+                            roomTGX.Room_type = acs.room.ToUpper().Equals(
+                                Room_Type.Suite.ToString().ToUpper())
+                                ? Room_Type.Suite : Room_Type.Standard;
+                            hotelTGX.Rooms.Add(roomTGX);
+                        }
                     }
                 }
 
-                else if (nameof(mealPlan.hotel.ave).Equals(hotel.Code))
-                {
+                else if (nameof(mealPlan.hotel.ave).Equals(hotel.Code) &&
+                    roomType.code.Equals(mealPlan.hotel.ave.FirstOrDefault().room) 
+                ) {
                     roomTGX = new RoomTGX(roomType);
                     Ave ave = (Ave)mealPlan.hotel.ave.FirstOrDefault();
                     roomTGX.Price = ave.price;
                     roomTGX.Meals_plan = mealPlan.code;
                     roomTGX.Room_type = ave.room.Equals(Room_Type.Suite) ? Room_Type.Suite : Room_Type.Standard;
                     hotelTGX.Rooms.Add(roomTGX);
-                    // var itemToRemove = mealPlan.hotel.ave.Find(m=> m.price == ave.price && m.room == ave.room);
-                    // mealPlan.hotel.ave.Remove(itemToRemove);
                 }
 
-                iMealPlan++;
-                Debug.WriteLine("   GetMealPlansInRoomsAtalaya iMealPlan: " + iMealPlan);
             }
         }
 
@@ -165,27 +148,42 @@ namespace codeTestTgx.Controllers
         {
             // Get of hotels list in API Resort format
             HttpResponseMessage responseHotels = await client.GetAsync(pathHotelsApiResort);
-            if (responseHotels.IsSuccessStatusCode)
+            HttpResponseMessage responseRoomMPApiResort = await client.GetAsync(pathRoomMPApiResort);
+
+            if (responseHotels.IsSuccessStatusCode && responseRoomMPApiResort.IsSuccessStatusCode)
             {
                 RoomsApiResort apiResort = await responseHotels.Content.ReadAsAsync<RoomsApiResort>();
+                MealPlansApiResort mealPlansAR = await responseRoomMPApiResort.Content.ReadAsAsync<MealPlansApiResort>();
 
                 // Iteration to save room information in TravelgateX format
                 foreach (HotelApiResort hotel in apiResort.hotels)
                 {
                     HotelTGX hotelTGX = new HotelTGX(hotel.code, hotel.name, hotel.location);
-                    GetRoomsApiResort(hotelTGX, hotel);
+                    GetRoomsApiResort(hotelTGX, hotel, mealPlansAR);
                     hotelsTGX.hotels.Add(hotelTGX);
                 }
             }
 
         }
 
-        private void GetRoomsApiResort(HotelTGX hotelTGX, HotelApiResort hotel)
+        private void GetRoomsApiResort(HotelTGX hotelTGX, HotelApiResort hotel, MealPlansApiResort mealPlansAR)
         {
             foreach (RoomApiResort roomApiResort in hotel.rooms)
             {
-                RoomTGX roomTGX = new RoomTGX(roomApiResort);
-                hotelTGX.Rooms.Add(roomTGX);
+                foreach (Regimene mealPlanAr in mealPlansAR.regimenes)
+                {
+                    if (hotel.code.Equals(mealPlanAr.hotel) &&
+                        roomApiResort.code.Equals(mealPlanAr.room_type)
+                    )
+                    {
+                        RoomTGX roomTGX = new RoomTGX(roomApiResort);
+                        roomTGX.Price = mealPlanAr.price;
+                        roomTGX.Meals_plan = mealPlanAr.code;
+                        roomTGX.Room_type = mealPlanAr.room_type.Equals("su")
+                            ? Room_Type.Suite : Room_Type.Standard;
+                        hotelTGX.Rooms.Add(roomTGX);
+                    }
+                }
             }
         }
 
---
+++ codeTestTgx/Models/MealPlansApiResort.cs
@@ -0,0 +1,19 @@
+﻿namespace codeTestTgx.Models
+{
+    public class Regimene
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+        public string hotel { get; set; }
+        public string room_type { get; set; }
+        public int price { get; set; }
+    }
+
+    public class MealPlansApiResort
+    {
+        public List<Regimene> regimenes { get; set; }
+    }
+
+
+
+}
